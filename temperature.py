34d5b7b0a4c3f09ab24d087e3d6d998611a6827d
Marcin Gierdalski <mgierdal@gmail.com>
2016-10-18 01:14:10 +0000

    thermostate run

--- temperature.py
+++ temperature.py
@@ -1,3 +1,6 @@
+__version__ = 0.9 # test version
+__version__ = 1.0 # heat and cool study
+
 # Import Libraries
 
 import RPi.GPIO as GPIO
@@ -5,6 +8,10 @@ import RPi.GPIO as GPIO
 import os
 import glob
 import time
+import sqlite3 as sql
+import datetime
+
+DB = r'record.db'
 
 # initilaize OUT
 GPIO.setmode(GPIO.BCM)
@@ -19,15 +26,16 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
  
-# A function that reads the sensors data
+
 def read_temp_raw():
+  """ead temp data"""
   f = open(device_file, 'r') # Opens the temperature device file
   lines = f.readlines() # Returns the text
   f.close()
   return lines
  
-# Convert the value of the sensor into a temperature
 def read_temp():
+  """convert raw readout into a temperature"""
   lines = read_temp_raw() # Read the temperature 'device file'
  
   # While the first line does not contain 'YES', wait for 0.2s
@@ -48,30 +56,88 @@ def read_temp():
     temp_f = temp_c * 9.0 / 5.0 + 32.0
     return temp_c, temp_f
 
-INTERVAL = 0.01 # seconds
+def keep_in_range(hi, lo, interval, period, gpio, cursor):
+    start_time = datetime.datetime.now()
+    ts = datetime.datetime.now()
+    while (ts - start_time) > period:
+        t_c, t_f = read_temp()
+        ts = datetime.datetime.now()
+        power_status = GPIO.input(GPIO_PIN)
+        if (t_c < lo):
+            if power_status == 0:
+                #print t_c, "LED switched on", ts
+                GPIO.output(gpio, 1)      # set GPIO to 1/GPIO.HIGH/True
+            else:
+                pass#print t_c, 'ON' if GPIO.input(gpio) == '1' else 'OFF', ts
+        elif (t_c > hi):
+            if power_status == 1:
+                #print t_c, "LED switched off", ts
+                GPIO.output(gpio, 0)         # set GPIO to 0/GPIO.LOW/False
+            else:
+                pass#print t_c, 'ON' if GPIO.input(gpio) == '1' else 'OFF', ts
+        else:
+            pass#print t_c, "in between", ts
+        power_status = GPIO.input(gpio)
+        c.execute("INSERT INTO trace VALUES (?,?,?)",(ts, t_c, power_status))
+        conn.commit()
+        time.sleep(INTERVAL)    # wait 
+        
+def blink(n, gpio, interval):
+    for i in range(n):
+        GPIO.output(gpio, 1)
+        #print 'blink ):'
+        time.sleep(interval)
+        GPIO.output(gpio, 0)
+        time.sleep(interval)
+
+
+INTERVAL = 60.0 # seconds
 THRESH_LO = 23
 THRESH_HI = 27
+DROP_DATA = True
+GPIO_PIN = 17
+
+start_time = datetime.datetime.now()
+heat_period = datetime.timedelta(0, 4.5 *60*60)
+cool_period = datetime.timedelta(0, 5.0 *60*60)
+
+conn = sql.connect(DB)
+c = conn.cursor()
+
+if DROP_DATA:
+    query = 'DROP TABLE IF EXISTS trace'
+    c.execute(query)
+    # Create table
+    query = 'CREATE TABLE trace (timestamp text, temp_c real, power_status integer)'
+    c.execute(query)
+
+blink(3, 17, 0.5)
 
 try:  
-    while True:
+    #keep_in_range(THRESH_HI, THRESH_LO, INTERVAL, 17, c)
+    GPIO.output(GPIO_PIN, 1)
+    ts = datetime.datetime.now()
+    while (ts - start_time) < heat_period:
+        if GPIO.input(GPIO_PIN) == 0:
+            GPIO.output(GPIO_PIN, 1)
+            #print 'f',
         t_c, t_f = read_temp()
-        if (t_c > THRESH_HI):
-            if not GPIO.input(17):
-                print t_c, "LED switched on"
-                GPIO.output(17, 1)      # set GPIO24 to 1/GPIO.HIGH/True
-            else:
-                print t_c, 'ON'
-        elif (t_c < THRESH_LO):
-            if GPIO.input(17):
-                print t_c, "LED switched off"
-                GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False
-            else:
-                print t_c, 'OFF'
-        else:
-            print t_c, "in between"
-        time.sleep(INTERVAL)                 # wait     
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+        ts = datetime.datetime.now()
+        power_status = GPIO.input(GPIO_PIN)
+        c.execute("INSERT INTO trace VALUES (?,?,?)",(ts, t_c, power_status))
+        conn.commit()
+        #print '.',
+        time.sleep(INTERVAL)    # wait 
+        keep_in_range(80, 80, INTERVAL, datetime.timedelta(0, 2 *60*60), 17, c)
+        keep_in_range(80, 76, INTERVAL, datetime.timedelta(0, 2 *60*60), 17, c)
+        keep_in_range(76, 60, INTERVAL, datetime.timedelta(0, 2 *60*60), 17, c)
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt
+    pass
+finally:
     GPIO.cleanup()
+    if conn:
+        conn.commit()
+        conn.close()
     
 # 46 01 4b 46 7f ff 0a 10 85 : crc=85 YES
 # 46 01 4b 46 7f ff 0a 10 85 t=20375
